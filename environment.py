b66a04bcd5e8edbbfece71ceea477594bdc35876
kaka <64834831+Djiango-Up@users.noreply.github.com>
2025-06-07 15:19:08 +0800

    Update environment.py

--- environment.py
+++ environment.py
@@ -4,34 +4,31 @@ from gym import spaces
 import numpy as np
 import torch
 
-#原tau_QoE思路：计算每个文件在每个AP的传输延迟-》对每个文件找到最小的传输延迟-》根据请求和权重更新总的QoE
 
-#cache是一个二维数组记载每个AP缓存的每个文件的量cache[i][j] 表示第i个AP的第j个文件的量，req请求向量，每个文件的请求次数，cap每个接入点的缓存容量
-#file文件总数，dis接入点之间的距离，weight文件权重向量，need用户设备当前需要的文件索引
 def tau_QoE(cache, req, cap, file, AP, file_size, dis, weight, need, UE,alpha):
-    SE = 200                                 #后续在调整QoE加入延迟考虑 应该调整这个SE 使得延迟敏感提高
+    SE = 200                                
     satisfy = np.zeros((UE, 1))
 
     T = np.array([1, 20])
-    taus = np.zeros((AP, file))              #计算每个文件在每个接入点AP的传输延迟
-    #np.sum(cache, axis=0).reshape((file, 1) 取cache第0轴求和，得到每个文件在所有接入点的总缓存量，再reshape成一个列向量
+    taus = np.zeros((AP, file))             
+    #np.sum(cache, axis=0).reshape((file, 1) 
     # print(file_size)
     # print(file.shape())
-    a = np.maximum(file_size - np.sum(cache, axis=0).reshape((file, 1)) * cap, np.zeros_like(file_size)) #计算每个文件中未被缓存的部分 并计算基本延迟
-    for i in range(file):                   #循环便利每个文件 对于每个AP 计算距离和缓存状态的延迟 更新taus数组
-        taus[:, i] = -a[i] * T[1]           #a[i]是文件i未被缓存的大小 T[]是延迟参数，延迟和未缓存的数据量成反比
+    a = np.maximum(file_size - np.sum(cache, axis=0).reshape((file, 1)) * cap, np.zeros_like(file_size))
+    for i in range(file):                   
+        taus[:, i] = -a[i] * T[1]          
         for j in range(AP):
-            distance = dis * abs(np.arange(AP) - j)         #计算从当前接入点AP到其他接入点的距离，dis是距离参数，-j表示计算每个接入点到当前接入点j的距离绝对值
-            for num in range(AP):                           #遍历可能缓存的接入点
-                b = int(min(AP - 1, j + num))               #b考虑缓存片段的结束索引
-                c = int(max(0, j - num))                    #c考虑缓存片段的开始索引
-                if cache[c:b + 1, i].sum() * cap + a[i] >= file_size[i]:    #计算当前接入点j是否有足够片段来存入缓存
-                    taus[j, i] = taus[j, i] - np.max(cache[c:b + 1, i] * distance[c:b + 1]) * cap * T[0]   #文件i在接入点j的传输延迟-接入点j到c-b之间最远的缓存片段延迟
+            distance = dis * abs(np.arange(AP) - j)         
+            for num in range(AP):                           
+                b = int(min(AP - 1, j + num))              
+                c = int(max(0, j - num))                    
+                if cache[c:b + 1, i].sum() * cap + a[i] >= file_size[i]:    
+                    taus[j, i] = taus[j, i] - np.max(cache[c:b + 1, i] * distance[c:b + 1]) * cap * T[0]  
                     break
 
-    single = np.zeros((file, 1))             #文件的最小传输延迟
+    single = np.zeros((file, 1))             
     file_delay = np.zeros((file, 1))
-    #存储每个文件的最小传输延迟
+    
     for i in range(file):
         single[i] = np.min(taus[:, i])
 
@@ -47,14 +44,13 @@ def tau_QoE(cache, req, cap, file, AP, file_size, dis, weight, need, UE,alpha):
         t = t - req[i] * (1 / SE)
 
         if t > -500:
-            QoE = QoE + req[i] + weight[i]                  #源码
-            satisfy[np.where(need == i)] = 1                #satify记录请求是否满足
+            QoE = QoE + req[i] + weight[i]                 
+            satisfy[np.where(need == i)] = 1               
             weight[i] = 0
-            # delay = delay + single[i] * (req[i] + weight[i])  # 加入了延迟权重项作为奖励函数的一部分，延迟和文件传输延迟、请求次数、文件权重相关
+            # delay = delay + single[i] * (req[i] + weight[i])  
             delay = delay + single[i]
         else:
-            weight[i] = weight[i] + req[i]                   #这段请求未能在可接受的延迟内得到满足，增加文件权重，提高缓存满足概率概率
-            #对于未满足的请求，延迟应该更大，考虑这个文件的请求次数、文件权重、大小
+            weight[i] = weight[i] + req[i]                 
             delay_pro = weight[i] + req[i] + file_size[i]
             delay = delay + delay_pro
 
@@ -70,12 +66,12 @@ def hit_2(cache, F, cap, file_size, req, K):
         r = r + req[f] * a[f]   #a[]是该文件的缓存比例
     return r / K
 
-#返回值可以算到能够满足的额外请求数量
-def hit_pro(cache, F, cap, file_size, req, K):          #K是UE总数
+
+def hit_pro(cache, F, cap, file_size, req, K):          
     r = 0
     for f in range(F):
-        if cache[:, f].sum() * cap >= file_size[f]:     #cache[:, f]，所有接入点缓存的第 f 个文件的总量。
-            r = r + req[f]                              #req每个文件的请求次数
+        if cache[:, f].sum() * cap >= file_size[f]:    
+            r = r + req[f]                              
     return r - K
 
 
@@ -120,8 +116,7 @@ class highway_cf(gym.Env, ABC):
         for j in range(self.F):
             self.zipf[j] = ((self.file[j] + 1) ** (-self.epsilon))
         a = self.zipf.sum()
-        self.zipf = self.zipf / a       #每个文件的 Zipf 值除以总和 a，得到该文件被请求的概率。
-
+        self.zipf = self.zipf / a      
     def request(self):
 
         #更新请求计数器，用于用来动态调整延迟因子 tau_QoE 中 alpha
